e4fa4f2b2eb8b81750c6c0abeed87eb181c8b125
meistercraft <njmeister99@gmail.com>
2024-05-08 18:24:33 -0500

    update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			title: 'Webpack Plugin',
+		}),
+		new WebpackPwaManifest({
+			name: 'Text Editor',
+			short_name: 'JATE',
+			description: 'A PWA text editor',
+			background_color: '#7eb4e2',
+			theme_color: '#7eb4e2',
+			start_url: './',
+			publicPath: '/',
+			icons: [
+			  {
+				src: path.resolve('favicon.ico'),
+				sizes: [96, 128, 192, 256, 384, 512],
+			  },
+			],
+		}),
+    	new InjectManifest({
+			swSrc: './src-sw.js',
+			swDest: 'service-worker.js',
+		}),
     ],
 
     module: {
       rules: [
-        
+		{
+        test: /\.css$/,
+		use: ['style-loader', 'css-loader'],
+		},
+		{
+		test: /\.js$/,
+		exclude: /node_modules/,
+		use: {
+		  loader: 'babel-loader',
+		},
+	  },
       ],
     },
   };
